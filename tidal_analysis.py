cf36fafefbf6692272cb73eb5ce934820baec191
harleymickleburgh <hhk532@york.ac.uk>
2026-05-05 13:05:21 +0100

    3 tests passed, 3 more functions done

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -1,4 +1,4 @@
-# import the modules we need
+#import the modules we need
 import pandas as pd
 import datetime
 import os
@@ -14,32 +14,32 @@ import argparse
 def read_tidal_data(filename):
     #skip the stuff at the top of the file
     tide_data = pd.read_csv(filename, skiprows=11, sep=r'\s+', header=None)
-    
+
     #Combine the date and time strings
     datetime_str = tide_data[1] + ' ' + tide_data[2]
     tide_data['Date'] = pd.to_datetime(datetime_str)
-    
-    #rename column 3 to Tide
+
+    #rename column 3 to Sea Level
     tide_data = tide_data.rename(columns={3: "Sea Level"})
-    
+
     #converting stuff to numbers
     tide_data['Sea Level'] = pd.to_numeric(tide_data['Sea Level'], errors= 'coerce')
-    
-    #set index and keep Tide column
+
+    #set index and keep Sea Level column
     tide_data = tide_data.set_index('Date')
     tide_data = tide_data[['Sea Level']]
 
     #hide sensor errors
-    tide_data = tide_data.mask(tide_data['Sea Level'] < -300)    
+    tide_data = tide_data.mask(tide_data['Sea Level'] < -300)
 
     return tide_data
-    
+
 def extract_single_year_remove_mean(year, data):
     #defining the time range
     year_string_start = str(year)+"0101"
     year_string_end = str(year)+"1231"
     year_data = data.loc[year_string_start:year_string_end, ['Sea Level']]
-    
+
     #averaging out the sea level
     mmm = np.mean(year_data['Sea Level'])
     year_data['Sea Level'] -= mmm
@@ -48,17 +48,31 @@ def extract_single_year_remove_mean(year, data):
 
 
 def extract_section_remove_mean(start, end, data):
+    #take only rows between start and end dates
+    section_data = data.loc[start:end, ['Sea Level']]
 
-    return year_data
+    #calculate the mean sea level for this section
+    section_mean = section_data['Sea Level'].mean()
 
+    #subtract mean from every value in section
+    section_data['Sea Level'] = section_data['Sea Level'] - section_mean
+
+    return section_data
 
 def join_data(data1, data2):
+    combined = pd.concat([data1, data2])
+    combined = combined.sort_index()
 
-    return 
+    return combined
 
 def sea_level_rise(data):
+    #convert the index to numbers
+    x_data = (data.index - data.index[0]).total_seconds() / (24 *3600)
+    
+    #regression on time(x) and sea level (y)
+    result = stats.linregress(x_data, data['Sea Level'])
 
-    return
+    return result.slope, result.pvalue
 
 def tidal_analysis(data, constituents, start_datetime):
 
@@ -66,7 +80,7 @@ def tidal_analysis(data, constituents, start_datetime):
 
 def get_longest_contiguous_data(data):
 
-    return 
+    return
 
 
 def main(args_list=None):
@@ -88,7 +102,6 @@ def main(args_list=None):
     verbose = args.verbose
 
     print("Add your code here to do things!")
-    
 
 if __name__ == '__main__':
     main()
