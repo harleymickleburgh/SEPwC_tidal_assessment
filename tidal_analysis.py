749be66078fc16c77487a6d76f2229476473368e
harleymickleburgh <hhk532@york.ac.uk>
2026-05-07 20:36:23 +0100

    cleaned up functions and i think things work 4 passed

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -1,4 +1,4 @@
-#import the modules we need
+"""Module for analysing tidal data"""
 import pandas as pd
 import datetime
 import os
@@ -12,6 +12,7 @@ import argparse
 
 
 def read_tidal_data(filename):
+    """reads the data and turns it into a useable format """
     #skip the stuff at the top of the file
     tide_data = pd.read_csv(filename, skiprows=11, sep=r'\s+', header=None)
 
@@ -19,24 +20,24 @@ def read_tidal_data(filename):
     datetime_str = tide_data[1] + ' ' + tide_data[2]
     tide_data['Time'] = pd.to_datetime(datetime_str)
 
-    #rename column 3 to Sea Level
-    tide_data = tide_data.rename(columns={3: "Sea Level"})
-
-    #converting stuff to numbers
+    #convert sea level and remove letters and stuff from numbers so the maths works
+    tide_data['Sea Level'] = tide_data[3].replace(r'[^0-9.-]', '', regex=True)
     tide_data['Sea Level'] = pd.to_numeric(tide_data['Sea Level'], errors= 'coerce')
 
+    #set outliers to NaN to be removed later
+    tide_data['Sea Level'] = tide_data['Sea Level'].mask(tide_data['Sea Level'] < -10)
+
     #set index but keep time and sea level column for test
-    tide_data.set_index('Time', inplace=True, drop=False)
+    tide_data.set_index('Time', inplace=True)
+    
+    resampled_data = tide_data[['Sea Level']].resample('h').mean()
+    
+    resampled_data['Time'] = resampled_data.index
 
-    #set index and keep Sea Level column
-    tide_data = tide_data[['Sea Level', 'Time']]
-
-    #hide sensor errors
-    tide_data = tide_data.mask(tide_data['Sea Level'] < -300)
-
-    return tide_data
+    return resampled_data[['Sea Level', 'Time']]
 
 def extract_single_year_remove_mean(year, data):
+    """groups data from single year to calculate a yearly average"""
     #defining the time range
     year_string_start = str(year)+"0101"
     year_string_end = str(year)+"1231"
@@ -50,8 +51,9 @@ def extract_single_year_remove_mean(year, data):
 
 
 def extract_section_remove_mean(start, end, data):
+    """calculates the actual height of each wave without sea level """
     #take only rows between start and end dates
-    section_data = data.loc[start:end, ['Sea Level']]
+    section_data = data.loc[start:end, ['Sea Level']].copy()
 
     #calculate the mean sea level for this section
     section_mean = section_data['Sea Level'].mean()
@@ -62,22 +64,25 @@ def extract_section_remove_mean(start, end, data):
     return section_data
 
 def join_data(data1, data2):
+    """combines data to allow for easier analysis"""
     combined = pd.concat([data1, data2])
     combined.sort_index(inplace=True)
 
     return combined
 
 def sea_level_rise(data):
+    """calculates the sea level rise from year to year"""
     #remove rows where sea level data is missing
     clean_data = data.dropna(subset=['Sea Level'])
 
-    #convert the index to numbers
-    x_data = (clean_data.index - clean_data.index[0]).total_seconds() / 86400
- 
-    #regression on time(x) and sea level (y)
-    result = stats.linregress(x_data, clean_data['Sea Level'])
+    #calculate days since first data point
+    x_data = (clean_data.index - clean_data.index[0]).total_seconds().values / 86400.0
+    y_data = clean_data['Sea Level'].values
 
-    return result.slope, result.pvalue
+    #regression for slope and intercept
+    slope, _, _, p, _ = stats.linregress(x_data, y_data)
+
+    return slope, p
 
 def tidal_analysis(data, constituents, start_datetime):
     """harmonic analysis on sea level data using specific consituents""" 
@@ -102,6 +107,7 @@ def tidal_analysis(data, constituents, start_datetime):
     return amp, pha
 
 def get_longest_contiguous_data(data):
+    """finds longest time with continous data"""
 
     return
 
